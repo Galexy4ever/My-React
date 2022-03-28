bde98fcdd970d30f222b73fdcff642c1e1d63145
Galexy4ever <mctasb@yandex.ru>
2022-03-28 23:03:11 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import './App.sass';
+
+
+
+function App() {
+    return (
+    <div className="Text">
+        Message
+    
+    </div>
+   
+    
+    );
+  
+}
+
+export default App;
